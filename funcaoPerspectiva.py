00463873ad06c16117003885a012731482819a18
unknown <d4vidcavalcante@gmail.com>
2019-11-28 11:36:37 -0300

    funcao que entra imagem sem perspectiva, le arquivo txt com os pontos de referencia para perspectiva e retorna imagem em perspectiva

---
+++ funcaoPerspectiva.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+def funcaoPerspectiva(imagem):
+    
+    imagem = cv2.imread('fotos_escaninho/escaninho0.jpeg')
+    cols = 800
+    rows = 600
+
+    pontosPerspectiva = np.loadtxt('pontosPerspectiva.txt')
+    pts1 = np.float32([pontosPerspectiva[0],pontosPerspectiva[1],pontosPerspectiva[2],pontosPerspectiva[3]])
+    pts2 = np.float32([[0,0],[cols,0],[cols,rows],[0,rows]])
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    perspectiva = cv2.warpPerspective(imagem,
+                                      M,
+                                      (cols,rows))
+
+    return (perspectiva)
